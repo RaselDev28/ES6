0a6d7f4a52322a2c7821fc43efa3c269accaa5e3
RaselDev28 <rasel.dev28@gmail.com>
2025-05-28 00:08:07 +0600

    template

---
+++ templete.js
@@ -0,0 +1,15 @@
+const a = 10;
+const b = 30;
+
+// const math = `The sum of ${a} and ${b} is ${a+b}`;
+
+const numbers =[45, 55, 54];
+const student ={name: 'Rasel', age: 45}
+const math = `The sum of ${numbers[1]} and ${student.age} is ${numbers[1] + student.age}`;
+
+console.log (math);
+
+const challenge = `Hi Jhon
+Cena kemon aco
+mair dibo onek`
+console.log(challenge)
